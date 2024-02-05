d8a58ebaabb247e332c0cf3d03e2269166dd8cfd
martindeibe02 <martindeibe@gmail.com>
2024-02-05 18:15:12 +0100

    Arreglado imagen nasa

--- bot_mdm/utils/api_scripts/api_nasa.py
+++ bot_mdm/utils/api_scripts/api_nasa.py
@@ -10,6 +10,6 @@ def get_api_apod():
     url_nasa = "https://api.nasa.gov/planetary/apod"
     payload = {"api_key": NASA_TOKEN}
     response = requests.get(url_nasa, params=payload)
-    url_apod, desc_apod, title = response.json()['hdurl'], response.json()['explanation'], response.json()['title']
+    url_apod, desc_apod, title = response.json()['url'], response.json()['explanation'], response.json()['title']
 
     return f"*{title}*", desc_apod, url_apod
