83ce67554cbdde81b05b19dae2f71d05ea36ab41
martindeibe02 <martindeibe@gmail.com>
2024-02-07 21:43:46 +0100

    Cambios en movies y creada news

--- bot_mdm/utils/scrap_scripts/scrap_movies.py
+++ bot_mdm/utils/scrap_scripts/scrap_movies.py
@@ -6,8 +6,11 @@ def get_movies():
     url_movies = "https://www.imdb.com/showtimes/cinema/ES/ci17723570/ES/15368"
     response = requests.get(url_movies)
     soup = BeautifulSoup(response.text, 'html.parser')
+    
+    index = url_movies.find('/', 8)
+    base_url = url_movies[:index]
 
-    movie_keys = ['Titulo', 'Estreno', 'Duracion', 'Genero', 'UserRating', 'Rank', 'Horas']
+    movie_keys = ['Titulo', 'Estreno', 'Duracion', 'Genero', 'UserRating', 'Rank', 'Horas', 'link']
 
     list_movies = [
             {
@@ -18,6 +21,7 @@ def get_movies():
                 movie_keys[4]: ' '.join(movie.find(class_ = "rating_txt").get_text(strip = True).split(" ")[:2]),
                 movie_keys[5]: movie.find_all(class_ = "nobr")[-1].get_text(strip = True),
                 movie_keys[6]: movie.find(class_="showtimes").get_text(strip = True).split("|"),
+                movie_keys[7]: base_url+movie.find(class_="info").find("a").get("href")
             }
             for movie in soup.findAll(True, {'class':['list_item odd', 'list_item even']})
     ]
@@ -29,7 +33,7 @@ def get_movies():
 
     for movie_data in top_5_movies:
         formatted_movie = f'''
-🎬 *Titulo:* {movie_data['Titulo']} {movie_data['Estreno']}
+🎬 *Titulo:* [{movie_data['Titulo']}]({movie_data['link']}) {movie_data['Estreno']}
 ⌛ *Duracion:* {movie_data['Duracion']}
 🌐 *Ranking:* {movie_data['Rank'].split(":")[1]}
 🌟 *UserRating:* {movie_data['UserRating'].split(":")[1]}
